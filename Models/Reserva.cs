9974241c36697f41f8c6982739578cb3bb93c5cc
Geisyla <limageisyla@gmail.com>
2024-05-01 13:23:59 -0300

    realizando as alterações pedidas

---
+++ Models/Reserva.cs
@@ -0,0 +1,58 @@
+namespace SISTEMADEHOSPEDAGEM.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                try{
+                    throw new("Capacidade menor que o número de hospedes");
+                }
+                catch(Exception ex )
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = valor * 0.1m;
+                valor -= desconto;
+            }
+
+            return valor;
+        }
+    }
+}
